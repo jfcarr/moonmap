eb672b795189847b929a8f562d735798e4c284bd
Jim Carr <jim.carr@devtoprd.com>
2024-09-20 21:10:12 -0400

    Added Wikipedia links to markers.

--- www/main.js
+++ www/main.js
@@ -1,39 +1,39 @@
 const craterList = [
-    ["Archimedes", 314.78125, 513.9375, ""],
-    ["Aristarchus", 123.15625, 447.734375, ""],
-    ["Byrgius", 116.875, 179.75, ""],
-    ["Copernicus", 245.125, 394.6875, ""],
-    ["Eratosthenes", 290.28125, 429.125, ""],
-    ["Grimaldi", 68, 275.5, ""],
-    ["Kepler", 158.875, 370.46875, ""],
-    ["Langrenus", 638, 355.625, ""],
-    ["Plato", 284.15625, 599.5625, ""],
-    ["Posidonius", 464.875, 548.5625, ""],
-    ["Stevinus", 601.90625, 224.6875, ""],
-    ["Tycho", 351.25, 133.75, ""],
+    ["Archimedes", 314.78125, 513.9375, "", "https://en.wikipedia.org/wiki/Archimedes_(crater)"],
+    ["Aristarchus", 123.15625, 447.734375, "", "https://en.wikipedia.org/wiki/Aristarchus_(crater)"],
+    ["Byrgius", 116.875, 179.75, "", "https://en.wikipedia.org/wiki/Byrgius_(crater)"],
+    ["Copernicus", 245.125, 394.6875, "", "https://en.wikipedia.org/wiki/Copernicus_(lunar_crater)"],
+    ["Eratosthenes", 290.28125, 429.125, "", "https://en.wikipedia.org/wiki/Eratosthenes_(crater)"],
+    ["Grimaldi", 68, 275.5, "", "https://en.wikipedia.org/wiki/Grimaldi_(crater)"],
+    ["Kepler", 158.875, 370.46875, "", "https://en.wikipedia.org/wiki/Kepler_(lunar_crater)"],
+    ["Langrenus", 638, 355.625, "", "https://en.wikipedia.org/wiki/Langrenus_(crater)"],
+    ["Plato", 284.15625, 599.5625, "", "https://en.wikipedia.org/wiki/Plato_(crater)"],
+    ["Posidonius", 464.875, 548.5625, "", "https://en.wikipedia.org/wiki/Posidonius_(crater)"],
+    ["Stevinus", 601.90625, 224.6875, "", "https://en.wikipedia.org/wiki/Stevinus_(crater)"],
+    ["Tycho", 351.25, 133.75, "", "https://en.wikipedia.org/wiki/Tycho_(lunar_crater)"],
 ];
 
 const mariaList = [
-    ["Mare Crisium (Sea of Crises)", 600.125, 493.25, ""],
-    ["Mare Fecunditatis (Sea of Fecundity)", 606.75, 385.5, ""],
-    ["Mare Frigoris (Sea of Cold)", 288.25, 623.25, ""],
-    ["Mare Humorum (Sea of Humidity)", 196.5, 202.5, ""],
-    ["Mare Imbrium (Sea of Rains)", 250.25, 528.25, ""],
-    ["Mare Nectaris (Sea of Nectar)", 546.75, 305.5, ""],
-    ["Mare Nubium (Sea of Clouds)", 303.625, 231.25, ""],
-    ["Mare Serenitatis (Sea of Serenity)", 429.5, 512.5, ""],
-    ["Mare Tranquillitatus (Sea of Tranquility)", 494, 428.75, ""],
-    ["Mare Vaporum (Sea of Vapors)", 374.875, 436.5, ""],
-    ["Oceanus Procellarum (Ocean of Storms)", 107, 373, ""],
+    ["Mare Crisium (Sea of Crises)", 600.125, 493.25, "", "https://en.wikipedia.org/wiki/Mare_Crisium"],
+    ["Mare Fecunditatis (Sea of Fecundity)", 606.75, 385.5, "", "https://en.wikipedia.org/wiki/Mare_Fecunditatis"],
+    ["Mare Frigoris (Sea of Cold)", 288.25, 623.25, "", "https://en.wikipedia.org/wiki/Mare_Frigoris"],
+    ["Mare Humorum (Sea of Humidity)", 196.5, 202.5, "", "https://en.wikipedia.org/wiki/Mare_Humorum"],
+    ["Mare Imbrium (Sea of Rains)", 250.25, 528.25, "", "https://en.wikipedia.org/wiki/Mare_Imbrium"],
+    ["Mare Nectaris (Sea of Nectar)", 546.75, 305.5, "", "https://en.wikipedia.org/wiki/Mare_Nectaris"],
+    ["Mare Nubium (Sea of Clouds)", 303.625, 231.25, "", "https://en.wikipedia.org/wiki/Mare_Nubium"],
+    ["Mare Serenitatis (Sea of Serenity)", 429.5, 512.5, "", "https://en.wikipedia.org/wiki/Mare_Serenitatis"],
+    ["Mare Tranquillitatis (Sea of Tranquility)", 494, 428.75, "", "https://en.wikipedia.org/wiki/Mare_Tranquillitatis"],
+    ["Mare Vaporum (Sea of Vapors)", 374.875, 436.5, "", "https://en.wikipedia.org/wiki/Mare_Vaporum"],
+    ["Oceanus Procellarum (Ocean of Storms)", 107, 373, "", "https://en.wikipedia.org/wiki/Oceanus_Procellarum"],
 ];
 
 const apolloList = [
-    ["Apollo 11", 487.375, 387.25, "July 1969 - Mare Tranquillitatus"],
-    ["Apollo 12", 236.25, 322, "November 1969 - Oceanus Procellarum"],
-    ["Apollo 14", 267, 320.5, "February 1971 - Fra Mauro Highlands"],
-    ["Apollo 15", 358.5, 502.75, "August 1971 - Hadley Rille"],
-    ["Apollo 16", 460.75, 321.5, "April 1972 - Descartes Highlands"],
-    ["Apollo 17", 492.75, 494.75, "December 1972 - Taurus-Littrow Valley"],
+    ["Apollo 11", 487.375, 387.25, "July 1969 - Mare Tranquillitatus", "https://en.wikipedia.org/wiki/Apollo_11"],
+    ["Apollo 12", 236.25, 322, "November 1969 - Oceanus Procellarum", "https://en.wikipedia.org/wiki/Apollo_12"],
+    ["Apollo 14", 267, 320.5, "February 1971 - Fra Mauro Highlands", "https://en.wikipedia.org/wiki/Apollo_14"],
+    ["Apollo 15", 358.5, 502.75, "August 1971 - Hadley Rille", "https://en.wikipedia.org/wiki/Apollo_15"],
+    ["Apollo 16", 460.75, 321.5, "April 1972 - Descartes Highlands", "https://en.wikipedia.org/wiki/Apollo_16"],
+    ["Apollo 17", 492.75, 494.75, "December 1972 - Taurus-Littrow Valley", "https://en.wikipedia.org/wiki/Apollo_17"],
 ];
 
 function init() {
@@ -80,6 +80,10 @@ function loadMarkers(iconFileName, markerList) {
         if (row[3] != "") {
             popupText = popupText + ": " + row[3];
         }
+        if (row[4] != "") {
+            popupText = popupText + "<hr><a href='" + row[4] + "' target='_blank'>More info</a>"
+        }
+
         L.marker(xy(row[1], row[2]), { icon: markerIcon, alt: row[0] }).addTo(newLayerGroup).bindPopup(popupText).bindTooltip(row[0]);
     });
 
